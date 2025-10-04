a09a7e394a771db6cc04807fe1659f33a8ec17c4
Nikodimos Jemaneh <nikodimosjemaneh40@gmail.com>
2025-10-04 19:18:04 +0300

    feat: chapa plugin index"

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export const greet = () => {
-  console.log("hello");
+import type { BetterAuthPlugin } from "better-auth";
+
+export const chapaPlugin = () => {
+  // Inject Chapa client into all endpoints using factory pattern
+
+  return {
+    id: "chapa",
+    endpoints: {},
+  } satisfies BetterAuthPlugin;
 };
